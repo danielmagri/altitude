feb73a5cf5c02590fd6842aac5a1dcbb142490cd
Daniel Magri <danielmagri98@gmail.com>
2019-03-27 12:41:59 -0300

    Ação de concluir hábito feita

---
+++ lib/widgets/Habit.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class HabitWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+//        Navigator.push(context, MaterialPageRoute(builder: (_) {
+//          return HabitDetailsPage();
+//        }));
+      },
+      child: Container(
+        height: 138.0,
+        width: 100.0,
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: 70.0,
+              height: 70.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                color: Colors.red,
+                boxShadow: [
+                  new BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 3.0,
+                    spreadRadius: 1.0,
+                    offset: new Offset(1.0, 1.0),
+                  )
+                ],
+              ),
+              child: Icon(
+                Icons.fitness_center,
+                size: 42.0,
+              ),
+            ),
+            Text(
+              "Academia",
+              style: TextStyle(fontSize: 14, color: Colors.black, fontWeight: FontWeight.normal),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
