a96172de2b7d8f0b5963a8916507de8d768737d5
Daniel Magri <danielmagri98@gmail.com>
2019-07-03 14:51:59 -0300

    Exibir tutorial na criação

--- lib/controllers/DataPreferences.dart
+++ lib/controllers/DataPreferences.dart
@@ -20,30 +20,38 @@ class DataPreferences {
 
   static const String NAME = "USER_NAME";
   static const String SCORE = "USER_SCORE";
+  static const String HABIT_TUTORIAL = "HABIT_TUTORIAL";
 
   Future<bool> setName(String name) async {
     SharedPreferences sf = await prefs;
-
     return await sf.setString(NAME, name);
   }
 
   Future<String> getName() async {
     SharedPreferences sf = await prefs;
-
     return sf.getString(NAME);
   }
 
   Future<bool> setScore(int score) async {
     SharedPreferences sf = await prefs;
     int currentScore = await getScore();
-
     return await sf.setInt(SCORE, currentScore + score);
   }
 
   Future<int> getScore() async {
     SharedPreferences sf = await prefs;
     int score = sf.getInt(SCORE);
-
     return score == null ? 0 : score;
+  }
+
+  Future<bool> setHabitTutorial(bool value) async {
+    SharedPreferences sf = await prefs;
+    return await sf.setBool(HABIT_TUTORIAL, value);
+  }
+
+  Future<bool> getHabitTutorial() async {
+    SharedPreferences sf = await prefs;
+    bool value = sf.getBool(HABIT_TUTORIAL);
+    return value == null ? false : value;
   }
 }
