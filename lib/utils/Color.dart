c06f445795a9f81288052898d99df01b14c010ea
Daniel Magri <daniel.magri@toodoo.com.br>
2019-07-29 18:33:48 -0300

    Adicionado mais cores

--- lib/utils/Color.dart
+++ lib/utils/Color.dart
@@ -3,5 +3,13 @@ import 'package:flutter/material.dart';
 abstract class HabitColors {
   static const Color disableHabitCreation = Colors.grey;
 
-  static const List<Color> colors = [Colors.red, Colors.blue, Colors.green, Colors.orange, Colors.yellow];
+  static const List<Color> colors = [
+    Color.fromARGB(255, 244, 67, 54),
+    Color.fromARGB(255, 244, 142, 52),
+    Color.fromARGB(255, 255, 235, 59),
+    Color.fromARGB(255, 76, 175, 80),
+    Color.fromARGB(255, 34, 200, 234),
+    Color.fromARGB(255, 0, 102, 204),
+    Color.fromARGB(255, 152, 53, 152)
+  ];
 }
