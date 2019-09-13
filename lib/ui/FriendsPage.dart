eeea076709ac103d1f2913461a7418054d1b8c81
Daniel Magri <danielmagri98@gmail.com>
2019-09-12 22:02:00 -0300

    Tela de ranking feita

--- lib/ui/FriendsPage.dart
+++ lib/ui/FriendsPage.dart
@@ -25,14 +25,33 @@ class _FriendsPageState extends State<FriendsPage> {
         score: 23),
   ];
 
+  List<Person> personsOrdened;
+
   @override
   void initState() {
+    personsOrdened = persons.toList();
+    personsOrdened.sort((a, b) => -a.score.compareTo(b.score));
+    persons.sort((a, b) => a.name.compareTo(b.name));
+
     super.initState();
 
     WidgetsBinding.instance.addPostFrameCallback((_) async {
-        Util.dialogNavigator(context, LoginPage());
+      Util.dialogNavigator(context, LoginPage());
     });
   }
+
+
+    Widget _positionWidget(int position) {
+      if (position == 1) {
+        return Image.asset("assets/first.png", height: 30);
+      }else if (position == 2) {
+        return Image.asset("assets/second.png", height: 30);
+      }else if (position == 3) {
+        return Image.asset("assets/third.png", height: 30);
+      }else{
+        return Text("#$position", style: TextStyle(fontSize: 20),);
+      }
+    }
 
   @override
   Widget build(BuildContext context) {
@@ -128,9 +147,66 @@ class _FriendsPageState extends State<FriendsPage> {
                 );
               },
             ),
-            Text(
-              "Second",
-              style: TextStyle(color: Colors.black),
+            ListView.builder(
+              itemExtent: 75,
+              padding: const EdgeInsets.only(bottom: 80),
+              physics: BouncingScrollPhysics(),
+              itemCount: personsOrdened.length,
+              itemBuilder: (BuildContext ctxt, int index) {
+                return Column(
+                  children: <Widget>[
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 19),
+                        child: Row(
+                          children: <Widget>[
+                            _positionWidget(index + 1),
+                            SizedBox(width: 12),
+                            Expanded(
+                              child: Text(
+                                personsOrdened[index].name,
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 2,
+                                style: TextStyle(
+                                    fontSize: 17, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Column(
+                              mainAxisSize: MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: <Widget>[
+                                Text(
+                                  personsOrdened[index].getLevelText(),
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                                Text(
+                                  "${personsOrdened[index].score} Km",
+                                  style: TextStyle(fontWeight: FontWeight.w300),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 1,
+                      width: double.maxFinite,
+                      margin: const EdgeInsets.symmetric(horizontal: 16),
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Colors.transparent,
+                            Colors.black38,
+                            Colors.transparent
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
             ),
           ],
         ),
