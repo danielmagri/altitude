a2fbf0159d156ea8241cacf09daa6194b59558fd
Daniel Magri <danielmagri98@gmail.com>
2019-10-16 16:05:24 -0300

    Correções de erros

--- lib/ui/tutorialPage.dart
+++ lib/ui/tutorialPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:habit/ui/widgets/generic/DotsIndicator.dart';
 import 'package:habit/utils/Validator.dart';
@@ -21,6 +22,14 @@ class _TutorialPageState extends State<TutorialPage> {
   final _nameTextController = TextEditingController();
 
   int pageIndex = 0;
+
+  @override
+  initState() {
+    super.initState();
+
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+        statusBarColor: Colors.transparent));
+  }
 
   @override
   void dispose() {
@@ -51,7 +60,8 @@ class _TutorialPageState extends State<TutorialPage> {
       Navigator.pop(context);
     } else {
       pageIndex++;
-      _controller.nextPage(duration: const Duration(milliseconds: 300), curve: Curves.ease);
+      _controller.nextPage(
+          duration: const Duration(milliseconds: 300), curve: Curves.ease);
     }
   }
 
@@ -68,18 +78,27 @@ class _TutorialPageState extends State<TutorialPage> {
                 child: Text(
                   "Como podemos te chamar?",
                   textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white),
+                  style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
                 ),
               ),
               Flexible(
                 child: Center(
                   child: Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0, vertical: 16.0),
                     margin: const EdgeInsets.only(left: 20),
                     decoration: BoxDecoration(
                       color: Colors.white,
-                      borderRadius: BorderRadius.only(topLeft: Radius.circular(20), bottomLeft: Radius.circular(20)),
-                      boxShadow: <BoxShadow>[BoxShadow(blurRadius: 6, color: Colors.black.withOpacity(0.3))],
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          bottomLeft: Radius.circular(20)),
+                      boxShadow: <BoxShadow>[
+                        BoxShadow(
+                            blurRadius: 6, color: Colors.black.withOpacity(0.3))
+                      ],
                     ),
                     child: TextField(
                       controller: _nameTextController,
@@ -87,7 +106,8 @@ class _TutorialPageState extends State<TutorialPage> {
                       textCapitalization: TextCapitalization.words,
                       onEditingComplete: _nextTap,
                       style: TextStyle(fontSize: 18.0),
-                      decoration: InputDecoration.collapsed(hintText: "Seu nome"),
+                      decoration:
+                          InputDecoration.collapsed(hintText: "Seu nome"),
                     ),
                   ),
                 ),
@@ -162,7 +182,10 @@ class Initial extends StatelessWidget {
             alignment: Alignment.center,
             child: Text(
               "Bem-vindo",
-              style: TextStyle(color: Colors.white, fontSize: 26, fontWeight: FontWeight.bold),
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold),
             ),
           ),
           Expanded(
@@ -175,7 +198,10 @@ class Initial extends StatelessWidget {
                 decoration: BoxDecoration(
                     color: Color.fromARGB(255, 250, 250, 250),
                     borderRadius: BorderRadius.circular(50),
-                    boxShadow: <BoxShadow>[BoxShadow(blurRadius: 10, color: Colors.black.withOpacity(0.3))]),
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                          blurRadius: 10, color: Colors.black.withOpacity(0.3))
+                    ]),
                 child: SizedBox(
                   width: 200,
                   height: 190,
@@ -218,13 +244,17 @@ class CreateHabit extends StatefulWidget {
   _CreateHabitState createState() => _CreateHabitState();
 }
 
-class _CreateHabitState extends State<CreateHabit> with SingleTickerProviderStateMixin {
+class _CreateHabitState extends State<CreateHabit>
+    with SingleTickerProviderStateMixin {
   AnimationController _controller;
 
   @override
   void initState() {
-    _controller =
-        AnimationController(duration: const Duration(milliseconds: 1000), vsync: this, lowerBound: 0, upperBound: 0.03);
+    _controller = AnimationController(
+        duration: const Duration(milliseconds: 1000),
+        vsync: this,
+        lowerBound: 0,
+        upperBound: 0.03);
     _controller.addListener(() {
       setState(() {});
     });
@@ -251,7 +281,10 @@ class _CreateHabitState extends State<CreateHabit> with SingleTickerProviderStat
             alignment: Alignment.center,
             child: Text(
               "CRIAR UM HÁBITO",
-              style: TextStyle(color: Colors.white, fontSize: 26, fontWeight: FontWeight.bold),
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold),
             ),
           ),
           Expanded(
@@ -265,10 +298,14 @@ class _CreateHabitState extends State<CreateHabit> with SingleTickerProviderStat
                       fit: BoxFit.cover,
                     ),
                     borderRadius: BorderRadius.circular(50),
-                    boxShadow: <BoxShadow>[BoxShadow(blurRadius: 10, color: Colors.black.withOpacity(0.3))]),
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                          blurRadius: 10, color: Colors.black.withOpacity(0.3))
+                    ]),
                 child: LayoutBuilder(builder: (context, constraint) {
                   return Align(
-                    alignment: Alignment(1.15 + _controller.value, 1.55 + _controller.value),
+                    alignment: Alignment(
+                        1.15 + _controller.value, 1.55 + _controller.value),
                     child: Image.asset(
                       "assets/finger.png",
                       fit: BoxFit.contain,
@@ -306,7 +343,8 @@ class CompleteHabit extends StatefulWidget {
   _CompleteHabitState createState() => _CompleteHabitState();
 }
 
-class _CompleteHabitState extends State<CompleteHabit> with SingleTickerProviderStateMixin {
+class _CompleteHabitState extends State<CompleteHabit>
+    with SingleTickerProviderStateMixin {
   AnimationController _controller;
   Animation<double> positionYRocketAnimation;
   Animation<double> positionXRocketAnimation;
@@ -315,7 +353,8 @@ class _CompleteHabitState extends State<CompleteHabit> with SingleTickerProvider
 
   @override
   void initState() {
-    _controller = AnimationController(duration: const Duration(milliseconds: 3500), vsync: this);
+    _controller = AnimationController(
+        duration: const Duration(milliseconds: 3500), vsync: this);
 
     _controller.addListener(() {
       setState(() {});
@@ -396,7 +435,10 @@ class _CompleteHabitState extends State<CompleteHabit> with SingleTickerProvider
             alignment: Alignment.center,
             child: Text(
               "COMPLETAR O HÁBITO",
-              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold, color: Colors.white),
+              style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
             ),
           ),
           Expanded(
@@ -410,19 +452,24 @@ class _CompleteHabitState extends State<CompleteHabit> with SingleTickerProvider
                       fit: BoxFit.cover,
                     ),
                     borderRadius: BorderRadius.circular(50),
-                    boxShadow: <BoxShadow>[BoxShadow(blurRadius: 10, color: Colors.black.withOpacity(0.3))]),
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                          blurRadius: 10, color: Colors.black.withOpacity(0.3))
+                    ]),
                 child: LayoutBuilder(builder: (context, constraint) {
                   final sizeRocket = 0.29;
                   return Stack(
                     children: <Widget>[
                       Align(
-                        alignment: Alignment(positionXRocketAnimation.value, positionYRocketAnimation.value),
+                        alignment: Alignment(positionXRocketAnimation.value,
+                            positionYRocketAnimation.value),
                         child: SizedBox(
                           height: constraint.biggest.width * sizeRocket,
                           width: (constraint.biggest.width * sizeRocket) + 10,
                           child: Rocket(
                             size: Size(
-                                (constraint.biggest.width * sizeRocket) + 10, constraint.biggest.width * sizeRocket),
+                                (constraint.biggest.width * sizeRocket) + 10,
+                                constraint.biggest.width * sizeRocket),
                             color: AppColors.habitsColor[3],
                             state: RocketState.ON_FIRE,
                             fireForce: 1,
@@ -430,7 +477,8 @@ class _CompleteHabitState extends State<CompleteHabit> with SingleTickerProvider
                         ),
                       ),
                       Align(
-                        alignment: Alignment(positionXFingerAnimation.value, positionYFingerAnimation.value),
+                        alignment: Alignment(positionXFingerAnimation.value,
+                            positionYFingerAnimation.value),
                         child: Image.asset(
                           "assets/finger.png",
                           fit: BoxFit.contain,
@@ -476,7 +524,8 @@ class _ScoreState extends State<Score> with SingleTickerProviderStateMixin {
 
   @override
   void initState() {
-    _controller = AnimationController(duration: const Duration(milliseconds: 4000), vsync: this);
+    _controller = AnimationController(
+        duration: const Duration(milliseconds: 4000), vsync: this);
 
     _controller.addListener(() {
       setState(() {});
@@ -518,7 +567,10 @@ class _ScoreState extends State<Score> with SingleTickerProviderStateMixin {
             alignment: Alignment.center,
             child: Text(
               "EVOLUÇÃO DO HÁBITO",
-              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold, color: Colors.white),
+              style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
             ),
           ),
           Expanded(
@@ -532,15 +584,22 @@ class _ScoreState extends State<Score> with SingleTickerProviderStateMixin {
                       fit: BoxFit.cover,
                     ),
                     borderRadius: BorderRadius.circular(50),
-                    boxShadow: <BoxShadow>[BoxShadow(blurRadius: 10, color: Colors.black.withOpacity(0.3))]),
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                          blurRadius: 10, color: Colors.black.withOpacity(0.3))
+                    ]),
                 child: Align(
-                    alignment: Alignment(0, -0.41),
-                    child: Text(
-                      scoreAnimation.value.toInt().toString(),
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 47, fontWeight: FontWeight.bold, height: 0.2, color: Colors.black),
-                    ),
+                  alignment: Alignment(0, -0.41),
+                  child: Text(
+                    scoreAnimation.value.toInt().toString(),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 47,
+                        fontWeight: FontWeight.bold,
+                        height: 0.2,
+                        color: Colors.black),
                   ),
+                ),
               ),
             ),
           ),
