dbe3ead210b5e72c489e076fcdd9cca2bb4da183
Daniel Magri <danielmagri98@gmail.com>
2020-03-10 20:24:53 -0300

    Dialog do reminder feito

--- lib/ui/habitDetails/blocs/EditAlarmBloc.dart
+++ lib/ui/habitDetails/blocs/EditAlarmBloc.dart
@@ -1,45 +1,148 @@
 import 'dart:async';
 import 'package:altitude/controllers/HabitsControl.dart';
 import 'package:altitude/core/bloc/BlocBase.dart';
-import 'package:altitude/core/model/Pair.dart';
 import 'package:altitude/model/Habit.dart';
+import 'package:altitude/model/Reminder.dart';
+import 'package:altitude/services/FireAnalytics.dart';
 import 'package:altitude/ui/habitDetails/enums/ReminderType.dart';
 import 'package:altitude/ui/habitDetails/model/ReminderCard.dart';
+import 'package:altitude/ui/habitDetails/model/ReminderWeekday.dart';
 import 'package:altitude/utils/Color.dart';
-import 'package:flutter/material.dart' show Color, BuildContext;
+import 'package:flutter/material.dart' show BuildContext, Color, Theme, ThemeData, TimeOfDay, Widget, showTimePicker;
 
 class EditAlarmBloc extends BlocBase {
   final Habit habit;
-  final Function(int) callback;
+  final List<Reminder> reminders;
+  final Function(List<Reminder>) callback;
 
   Color get habitColor => AppColors.habitsColor[habit.color];
 
+  TimeOfDay reminderTime;
+
+  ReminderType currentTypeSelected = ReminderType.HABIT;
   final List<ReminderCard> reminderCards = [
     ReminderCard(ReminderType.HABIT, "Lembrar do hábito"),
     ReminderCard(ReminderType.CUE, "Lembrar do gatilho")
+  ];
+
+  List<ReminderWeekday> reminderWeekdays = [
+    ReminderWeekday(1, "D", false),
+    ReminderWeekday(2, "S", false),
+    ReminderWeekday(3, "T", false),
+    ReminderWeekday(4, "Q", false),
+    ReminderWeekday(5, "Q", false),
+    ReminderWeekday(6, "S", false),
+    ReminderWeekday(7, "S", false)
   ];
 
   // Reminder Card Type Selected
   StreamController<ReminderType> _reminderCardTypeSelectedStreamController = StreamController();
   Stream<ReminderType> get reminderCardTypeSelectedStream => _reminderCardTypeSelectedStreamController.stream;
 
-  // Suggestion List
-  StreamController<List<String>> _suggestionListStreamController = StreamController();
-  Stream<List<String>> get suggestionListStream => _suggestionListStreamController.stream;
+  // Reminder weekday selection
+  StreamController<List<ReminderWeekday>> _reminderWeekdaySelectionStreamController = StreamController();
+  Stream<List<ReminderWeekday>> get reminderWeekdaySelectionStream => _reminderWeekdaySelectionStreamController.stream;
 
-  EditAlarmBloc(this.habit, this.callback);
+  // Reminder Time
+  StreamController<TimeOfDay> _reminderTimeStreamController = StreamController();
+  Stream<TimeOfDay> get reminderTimeStream => _reminderTimeStreamController.stream;
+
+  EditAlarmBloc(this.habit, this.reminders, this.callback) {
+    if (reminders != null && reminders.length != 0) {
+      currentTypeSelected = ReminderType.values.firstWhere((type) => type.value == reminders[0].type);
+      reminderTime = TimeOfDay(hour: reminders[0].hour, minute: reminders[0].minute);
+      reminders.forEach((reminder) {
+        reminderWeekdays[reminder.weekday - 1].state = true;
+      });
+    } else {
+      reminderTime = TimeOfDay.now();
+    }
+  }
 
   @override
   void dispose() {
     _reminderCardTypeSelectedStreamController.close();
-    _suggestionListStreamController.close();
+    _reminderTimeStreamController.close();
+    _reminderWeekdaySelectionStreamController.close();
   }
 
   void switchReminderType(ReminderType type) {
+    if (type == ReminderType.CUE && habit.cue == "") {
+      showToast("Adicione o gatilho primeiro");
+      return;
+    }
+    currentTypeSelected = type;
     _reminderCardTypeSelectedStreamController.sink.add(type);
   }
 
-  void save() {}
+  void reminderWeekdayClick(int id, bool state) {
+    reminderWeekdays.firstWhere((item) => item.id == id)?.state = state;
 
-  void remove() {}
+    _reminderWeekdaySelectionStreamController.sink.add(reminderWeekdays);
+  }
+
+  void reminderTimeClick(BuildContext context) {
+    showTimePicker(
+      initialTime: reminderTime,
+      context: context,
+      builder: (BuildContext context, Widget child) {
+        return Theme(
+            data: ThemeData.light().copyWith(
+              accentColor: habitColor,
+              primaryColor: habitColor,
+            ),
+            child: child);
+      },
+    ).then((time) {
+      if (time != null) {
+        reminderTime = time;
+        _reminderTimeStreamController.sink.add(reminderTime);
+      }
+    });
+  }
+
+  void save(BuildContext context) async {
+    if (!reminderWeekdays.any((item) => item.state)) {
+      showToast("Selecione pelo menos um dia");
+    } else {
+      showLoading(context);
+      if (reminders.length != 0) {
+        await HabitsControl().deleteReminders(habit.id, reminders);
+      }
+      List<Reminder> newReminders = List();
+      String days = "";
+      reminderWeekdays.forEach((item) {
+        if (item.state) {
+          newReminders.add(Reminder(
+              habitId: habit.id,
+              hour: reminderTime.hour,
+              minute: reminderTime.minute,
+              weekday: item.id,
+              type: currentTypeSelected.value));
+          days += item.title;
+        } else {
+          days += "-";
+        }
+      });
+
+      await HabitsControl().addReminders(habit, newReminders);
+
+      FireAnalytics()
+          .sendSetAlarm(habit.habit, currentTypeSelected.value, reminderTime.hour, reminderTime.minute, days);
+      showToast("Alarme salvo");
+      hideLoading(context);
+
+      callback(newReminders);
+    }
+  }
+
+  void remove(BuildContext context) {
+    showLoading(context);
+    HabitsControl().deleteReminders(habit.id, reminders).then((status) {
+      hideLoading(context);
+      FireAnalytics().sendRemoveAlarm(habit.habit);
+      showToast("Alarme removido");
+      callback(List());
+    });
+  }
 }
--- lib/ui/habitDetails/dialogs/EditAlarmDialog.dart
+++ lib/ui/habitDetails/dialogs/EditAlarmDialog.dart
@@ -1,91 +1,69 @@
 import 'package:altitude/core/bloc/BlocProvider.dart';
 import 'package:altitude/core/bloc/BlocWidget.dart';
-import 'package:altitude/core/model/Pair.dart';
 import 'package:altitude/model/Habit.dart';
+import 'package:altitude/model/Reminder.dart';
 import 'package:altitude/ui/habitDetails/blocs/EditAlarmBloc.dart';
 import 'package:altitude/ui/habitDetails/enums/ReminderType.dart';
 import 'package:altitude/ui/habitDetails/model/ReminderCard.dart';
+import 'package:altitude/ui/habitDetails/model/ReminderWeekday.dart';
 import 'package:altitude/ui/widgets/generic/BottomSheetLine.dart';
 import 'package:flutter/material.dart';
 
 class EditAlarmDialog extends BlocWidget<EditAlarmBloc> {
-  static StatefulWidget instance(Habit habit, Function(int) callback) {
+  static StatefulWidget instance(Habit habit, List<Reminder> reminders, Function(List<Reminder>) callback) {
     return BlocProvider<EditAlarmBloc>(
-      blocCreator: () => EditAlarmBloc(habit, callback),
+      blocCreator: () => EditAlarmBloc(habit, reminders, callback),
       widget: EditAlarmDialog(),
     );
   }
 
-  Widget _reminderCard(EditAlarmBloc bloc, bool isSelected, ReminderCard item) {
-    return Expanded(
-      child: Card(
-        margin: const EdgeInsets.symmetric(vertical: 0, horizontal: 4),
-        elevation: 4,
-        color: isSelected ? bloc.habitColor : Colors.white,
-        child: SizedBox(
-          height: 60,
-          child: InkWell(
-            onTap: () => bloc.switchReminderType(item.type),
-              // if (errorMessage != null) {
-              //   showToast(errorMessage);
-              // } else {
-              //   setState(() {
-              //     _reminderType = index;
-              //   });
-              // }
-            // },
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: Row(
-                children: <Widget>[
-                  Radio(
-                    value: isSelected ? true : false,
-                    groupValue: true,
-                    activeColor: Colors.white,
-                    onChanged: (state) => bloc.switchReminderType(item.type),
-                  ),
-                  Expanded(
-                    child: Text(
-                      item.title,
-                      style: TextStyle(color: isSelected ? Colors.white : Colors.black),
+  Widget _reminderCard(EditAlarmBloc bloc, bool isSelected, ReminderCard item) => Expanded(
+        child: Card(
+          margin: const EdgeInsets.symmetric(vertical: 0, horizontal: 4),
+          elevation: 4,
+          color: isSelected ? bloc.habitColor : Colors.white,
+          child: SizedBox(
+            height: 60,
+            child: InkWell(
+              onTap: () => bloc.switchReminderType(item.type),
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: Row(
+                  children: <Widget>[
+                    Radio(
+                      value: isSelected ? true : false,
+                      groupValue: true,
+                      activeColor: Colors.white,
+                      onChanged: (state) => bloc.switchReminderType(item.type),
                     ),
-                  ),
-                ],
+                    Expanded(
+                      child: Text(
+                        item.title,
+                        style: TextStyle(color: isSelected ? Colors.white : Colors.black),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
         ),
-      ),
-    );
-  }
+      );
 
-  // List<Widget> _weekdayListWidget() {
-  //   List<Widget> widgets = List();
-
-  //   for (int i = 0; i < 7; i++) {
-  //     widgets.add(Expanded(
-  //       child: Container(
-  //         padding: const EdgeInsets.all(4),
-  //         decoration:
-  //             BoxDecoration(shape: BoxShape.circle, color: _weekdayStatus[i] ? bloc.habitColor : Colors.transparent),
-  //         child: InkWell(
-  //           onTap: () {
-  //             setState(() {
-  //               _weekdayStatus[i] = !_weekdayStatus[i];
-  //             });
-  //           },
-  //           child: Text(
-  //             _weekdayText[i],
-  //             textAlign: TextAlign.center,
-  //             style: TextStyle(fontSize: 18, color: _weekdayStatus[i] ? Colors.white : Colors.black),
-  //           ),
-  //         ),
-  //       ),
-  //     ));
-  //   }
-
-  //   return widgets;
-  // }
+  Widget _reminderWeekday(EditAlarmBloc bloc, ReminderWeekday item) => Expanded(
+        child: Container(
+          padding: const EdgeInsets.all(4),
+          decoration: BoxDecoration(shape: BoxShape.circle, color: item.state ? bloc.habitColor : Colors.transparent),
+          child: InkWell(
+            onTap: () => bloc.reminderWeekdayClick(item.id, !item.state),
+            child: Text(
+              item.title,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18, color: item.state ? Colors.white : Colors.black),
+            ),
+          ),
+        ),
+      );
 
   @override
   Widget build(BuildContext context, EditAlarmBloc bloc) {
@@ -113,204 +91,91 @@ class EditAlarmDialog extends BlocWidget<EditAlarmBloc> {
             ),
           ),
           StreamBuilder<ReminderType>(
-              initialData: ReminderType.CUE,
-              stream: bloc.reminderCardTypeSelectedStream,
-              builder: (context, snapshot) {
-                return Row(
-                  children:
-                      bloc.reminderCards.map((item) => _reminderCard(bloc, snapshot.data == item.type, item)).toList(),
-                );
-                // children: <Widget>[
-                //   _reminderIndexCard(0, "Lembrar do hábito"),
-                //   SizedBox(width: 8),
-                //   _reminderIndexCard(1, "Lembrar do gatilho",
-                //       errorMessage: DataHabitDetail().habit.cue == null ? "Adicione o gatilho primeiro" : null),
-                // ],
-              }),
-
-          // SizedBox(
-          //   height: 32,
-          // ),
-          // Padding(
-          //   padding: const EdgeInsets.all(8),
-          //   child: Text(
-          //     "Selecione os dias e o horário que deseja ser lembrado:",
-          //     style: TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
-          //   ),
-          // ),
-          // Row(
-          //   children: _weekdayListWidget(),
-          // ),
-          // Container(
-          //   margin: EdgeInsets.only(top: 24),
-          //   child: InkWell(
-          //     onTap: () {
-          //       showTimePicker(
-          //         initialTime: _temporaryTime == null ? TimeOfDay.now() : _temporaryTime,
-          //         context: context,
-          //         builder: (BuildContext context, Widget child) {
-          //           return Theme(
-          //               data: ThemeData.light().copyWith(
-          //                 accentColor: bloc.habitColor,
-          //                 primaryColor: bloc.habitColor,
-          //               ),
-          //               child: child);
-          //         },
-          //       ).then((time) {
-          //         if (time != null) {
-          //           setState(() {
-          //             _temporaryTime = time;
-          //           });
-          //         }
-          //       });
-          //     },
-          //     child: RichText(
-          //       text: TextSpan(
-          //         style: TextStyle(color: Colors.black, fontSize: 20, fontFamily: "Montserrat"),
-          //         children: <TextSpan>[
-          //           TextSpan(
-          //               text: _temporaryTime == null
-          //                   ? "-- : --"
-          //                   : _temporaryTime.hour.toString().padLeft(2, '0') +
-          //                       ":" +
-          //                       _temporaryTime.minute.toString().padLeft(2, '0'),
-          //               style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-          //           TextSpan(text: " hrs"),
-          //         ],
-          //       ),
-          //     ),
-          //   ),
-          // ),
-          // Spacer(
-          //   flex: 3,
-          // ),
-          // Container(
-          //   margin: const EdgeInsets.only(bottom: 16),
-          //   child: Row(
-          //     mainAxisAlignment: MainAxisAlignment.center,
-          //     children: <Widget>[
-          //       DataHabitDetail().reminders.length != 0
-          //           ? FlatButton(
-          //               onPressed: bloc.remove,
-          //               child: Text(
-          //                 "Remover",
-          //                 style: TextStyle(fontSize: 16),
-          //               ),
-          //             )
-          //           : SizedBox(),
-          //       RaisedButton(
-          //         color: bloc.habitColor,
-          //         shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(20.0)),
-          //         padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-          //         elevation: 0,
-          //         onPressed: bloc.save,
-          //         child: const Text(
-          //           "SALVAR",
-          //           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-          //         ),
-          //       ),
-          //     ],
-          //   ),
-          // ),
+            initialData: bloc.currentTypeSelected,
+            stream: bloc.reminderCardTypeSelectedStream,
+            builder: (context, snapshot) {
+              return Row(
+                children:
+                    bloc.reminderCards.map((item) => _reminderCard(bloc, snapshot.data == item.type, item)).toList(),
+              );
+            },
+          ),
+          SizedBox(
+            height: 32,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Text(
+              "Selecione os dias e o horário que deseja ser lembrado:",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+            ),
+          ),
+          StreamBuilder<List<ReminderWeekday>>(
+            initialData: bloc.reminderWeekdays,
+            stream: bloc.reminderWeekdaySelectionStream,
+            builder: (context, snapshot) {
+              return Row(
+                children: snapshot.data.map((item) => _reminderWeekday(bloc, item)).toList(),
+              );
+            },
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 24),
+            child: InkWell(
+              onTap: () => bloc.reminderTimeClick(context),
+              child: StreamBuilder<TimeOfDay>(
+                initialData: bloc.reminderTime,
+                stream: bloc.reminderTimeStream,
+                builder: (context, snapshot) {
+                  return RichText(
+                    text: TextSpan(
+                      style: TextStyle(color: Colors.black, fontSize: 20, fontFamily: "Montserrat"),
+                      children: <TextSpan>[
+                        TextSpan(
+                            text:
+                                "${snapshot.data.hour.toString().padLeft(2, '0')} : ${snapshot.data.minute.toString().padLeft(2, '0')}",
+                            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                        TextSpan(text: " hrs"),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+          Spacer(
+            flex: 3,
+          ),
+          Container(
+            margin: const EdgeInsets.only(bottom: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                bloc.reminders.length != 0
+                    ? FlatButton(
+                        onPressed: () => bloc.remove(context),
+                        child: Text(
+                          "Remover",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      )
+                    : SizedBox(),
+                RaisedButton(
+                  color: bloc.habitColor,
+                  shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(20.0)),
+                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                  elevation: 0,
+                  onPressed: () => bloc.save(context),
+                  child: const Text(
+                    "SALVAR",
+                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
 }
-
-// class _EditAlarmDialogState extends State<EditAlarmDialog> {
-//   final List<String> _weekdayText = ["D", "S", "T", "Q", "Q", "S", "S"];
-
-//   int _reminderType = 0;
-//   List<bool> _weekdayStatus;
-//   TimeOfDay _temporaryTime;
-
-//   @override
-//   initState() {
-//     super.initState();
-//     resetData();
-//   }
-
-//   @override
-//   void didUpdateWidget(EditAlarmDialog oldWidget) {
-//     resetData();
-//     super.didUpdateWidget(oldWidget);
-//   }
-
-//   void resetData() {
-//     _weekdayStatus = [false, false, false, false, false, false, false];
-
-//     if (DataHabitDetail().reminders.length == 0) {
-//       _reminderType = 0;
-//       _temporaryTime = null;
-//     } else {
-//       _reminderType = DataHabitDetail().reminders[0].type;
-//       _temporaryTime = new TimeOfDay(
-//           hour: DataHabitDetail().reminders[0].hour,
-//           minute: DataHabitDetail().reminders[0].minute);
-
-//       for (Reminder reminder in DataHabitDetail().reminders) {
-//         _weekdayStatus[reminder.weekday - 1] = true;
-//       }
-//     }
-//   }
-
-//   void _save() async {
-//     if (_temporaryTime == null) {
-//       showToast("Selecione um horário");
-//     } else if (!_weekdayStatus.contains(true)) {
-//       showToast("Selecione pelo menos um dia");
-//     } else {
-//       Loading.showLoading(context);
-//       if (DataHabitDetail().reminders.length != 0) {
-//         await HabitsControl().deleteReminders(
-//             DataHabitDetail().habit.id, DataHabitDetail().reminders);
-//       }
-
-//       List<Reminder> reminders = new List();
-//       String days = "";
-//       for (int i = 0; i < 7; i++) {
-//         if (_weekdayStatus[i]) {
-//           reminders.add(new Reminder(
-//               habitId: DataHabitDetail().habit.id,
-//               hour: _temporaryTime.hour,
-//               minute: _temporaryTime.minute,
-//               weekday: i + 1,
-//               type: _reminderType));
-//           days += _weekdayText[i];
-//         } else {
-//           days += "-";
-//         }
-//       }
-
-//       DataHabitDetail().reminders =
-//           await HabitsControl().addReminders(DataHabitDetail().habit, reminders);
-
-//       FireAnalytics().sendSetAlarm(DataHabitDetail().habit.habit, _reminderType,
-//           _temporaryTime.hour, _temporaryTime.minute, days);
-//       showToast("Alarme salvo");
-//       Loading.closeLoading(context);
-//       widget.closeBottomSheet();
-//     }
-//   }
-
-//   void _remove() {
-//     Loading.showLoading(context);
-//     HabitsControl()
-//         .deleteReminders(
-//             DataHabitDetail().habit.id, DataHabitDetail().reminders)
-//         .then((status) {
-//       Loading.closeLoading(context);
-//       DataHabitDetail().reminders = new List();
-
-//       FireAnalytics().sendRemoveAlarm(DataHabitDetail().habit.habit);
-//       showToast("Alarme removido");
-//       widget.closeBottomSheet();
-//     });
-//   }
-
-//
-
-//
-
-//
