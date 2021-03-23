a9143958aefa09a915254e59c97bbde0e12a4217
Daniel Magri <danielmagri98@gmail.com>
2021-03-23 15:47:10 -0300

    getHabits func unit test done

--- test/feature/home/home_logic_test.dart
+++ test/feature/home/home_logic_test.dart
@@ -1,4 +1,4 @@
-import 'package:altitude/common/model/Person.dart';
+import 'package:altitude/common/model/Habit.dart';
 import 'package:altitude/common/useCase/CompetitionUseCase.dart';
 import 'package:altitude/common/useCase/HabitUseCase.dart';
 import 'package:altitude/common/useCase/PersonUseCase.dart';
@@ -7,6 +7,7 @@ import 'package:altitude/core/model/Result.dart';
 import 'package:altitude/feature/home/logic/HomeLogic.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
+import 'package:mobx/mobx.dart' as mobx;
 import 'package:mockito/mockito.dart';
 import '../../config/init_config.dart';
 
@@ -22,14 +23,53 @@ void main() {
   });
 
   group('getHabits', () {
-    //TODO
-    // test('returns habits list', () async {
-    //   final logic = HomeLogic();
+    test('returns habits list', () async {
+      final habitUseCase = HabitUseCase.getI;
+      final List<Habit> data = [Habit()];
 
-    //   expect(logic.habits.state, StateType.INITIAL);
+      expect(logic.habits.state, StateType.INITIAL);
 
-    //   logic.getHabits();
-    // });
+      when(habitUseCase.getHabits()).thenAnswer((_) => Future.value(Result.success(data)));
+
+      logic.getHabits();
+
+      await mobx.asyncWhen((_) => logic.habits.state != StateType.INITIAL);
+
+      expect(logic.habits.state, StateType.SUCESS);
+      expect(logic.habits.data, data);
+    });
+
+    test('returns empty list', () async {
+      final habitUseCase = HabitUseCase.getI;
+      final List<Habit> data = [];
+
+      expect(logic.habits.state, StateType.INITIAL);
+
+      when(habitUseCase.getHabits()).thenAnswer((_) => Future.value(Result.success(data)));
+
+      logic.getHabits();
+
+      await mobx.asyncWhen((_) => logic.habits.state != StateType.INITIAL);
+
+      expect(logic.habits.state, StateType.SUCESS);
+      expect(logic.habits.data, data);
+    });
+
+    test('returns error', () async {
+      final habitUseCase = HabitUseCase.getI;
+      final dynamic error = "error";
+
+      expect(logic.habits.state, StateType.INITIAL);
+
+      when(habitUseCase.getHabits()).thenAnswer((_) => Future.value(Result.error(error)));
+
+      logic.getHabits();
+
+      await mobx.asyncWhen((_) => logic.habits.state != StateType.INITIAL);
+
+      expect(logic.habits.state, StateType.ERROR);
+      expect(logic.habits.error, error);
+    });
   });
 
   group('fetchPendingStatus', () {
@@ -48,7 +88,7 @@ void main() {
       expect(logic.pendingCompetitionStatus, false);
     });
 
-    test('returns true to pending competition status', () async {
+    test('returns true to pending competition status', () {
       final personUseCase = PersonUseCase.getI;
       final competitionUseCase = CompetitionUseCase.getI;
 
