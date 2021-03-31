30705019a5437d66b40568523535bc59704da989
Daniel Magri <danielmagri98@gmail.com>
2021-03-31 20:37:56 -0300

    version bugfix

--- lib/feature/login/logic/LoginLogic.dart
+++ lib/feature/login/logic/LoginLogic.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:injectable/injectable.dart';
 import 'package:mobx/mobx.dart';
 part 'LoginLogic.g.dart';
 
+@LazySingleton()
 class LoginLogic = _LoginLogicBase with _$LoginLogic;
 
 abstract class _LoginLogicBase with Store {
